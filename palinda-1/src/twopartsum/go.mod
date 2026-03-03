b2f55b0a55797c82e24f78442728cee52ba07471
lenarv <lenarv@ArvidsLenovo.localdomain>
2026-03-03 21:14:01 +0100

    compleated twopartsum task => palinda 1 done

---
+++ palinda-1/src/twopartsum/go.mod
@@ -0,0 +1,3 @@
+module module/twopartsum
+
+go 1.25.0
---
+++ palinda-1/src/twopartsum/twopartsum_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+// test that ConcurrentSum sums an even-length array correctly
+func TestSumConcurrentCorrectlySumsEvenArray(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expected := 55
+
+	actual := ConcurrentSum(arr)
+
+	if actual != expected {
+		t.Errorf("expected %d, was %d", expected, actual)
+	}
+}
+func TestSumConcurrentCorectlySumOddArray(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	expected := 45
+	actual := ConcurrentSum(arr)
+
+	if actual != expected {
+		t.Errorf("expected %d, was %d", expected, actual)
+	}
+}
+
+func TestSumConcurrentCorectlySumOneValue(t *testing.T) {
+	arr := []int{13}
+	expected := 13
+	actual := ConcurrentSum(arr)
+
+	if actual != expected {
+		t.Errorf("expected %d, was %d", expected, actual)
+	}
+}
+
+// TODO add at least two more test cases!
